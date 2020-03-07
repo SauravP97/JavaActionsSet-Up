2779e0895ed0bd602b0e18e848312a0bcbbe349b
Saurav Prateek <srvptk97@gmail.com>
2020-03-07 13:33:52 +0530

    Trying Again

--- .github/actions/code-checker/index.js
+++ .github/actions/code-checker/index.js
@@ -1,7 +1,7 @@
 console.log('Hello World');
 	const fs = require('fs')
 
-	fs.readFile('../../../Problems/p1.txt', (err, data) => {
+	fs.readFile('./././Problems/p1.txt', (err, data) => {
     		if (err) throw err;
     		console.log(data.toString());
 	})
