144a1232a81e160f37a4d2a519de568a23e13d4b
Saurav Prateek <srvptk97@gmail.com>
2020-03-08 09:06:04 +0530

    Added File Checker in Action

--- .github/actions/code-checker/index.js
+++ .github/actions/code-checker/index.js
@@ -1,13 +1,42 @@
 console.log('Hello World');
+
+function checkTestCase(){
+
 	const fs = require('fs')
+
+	var outputString = "";
+	var correctOutputString = "";
 
 	fs.readFile('./././Outputs/o1/o1_1.txt', (err, data) => {
     		if (err) throw err;
-    		console.log(data.toString());
+    		outputString = data.toString();
 	})
 
 	fs.readFile('./././correctOutputs/co1/co1_1.txt', (err, data) => {
                 if (err) throw err;
-                console.log(data.toString());
+		correctOutputString = data.toString();
         })
 
+	outputString = outputString.trim();
+	correctOutputString = correctOutputString.trim();
+
+	if(outputString.length != correctOutputString.length){
+		return "Test Case Failed":
+	}
+
+	var failed = false;
+	var i = 0;
+
+	while(i < correctOutputString.length){
+		var char1 = correctOutputString.charAt(i);
+		var char2 = outputString.charAt(i);
+		if(char1 != char2){
+			return "Test Case Failed";
+		}
+		i++;
+	}
+
+	return "Test Case Passes";
+}
+
+checkTestCase();
