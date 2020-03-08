e7be4507050dea1b9c707293a4fefd24b21b81c6
Saurav Prateek <srvptk97@gmail.com>
2020-03-08 13:59:51 +0530

    Testing Again

--- Solutions/s3/MySolution.java
+++ Solutions/s3/MySolution.java
@@ -4,20 +4,26 @@ import java.io.*;
 
 class MySolution{
         public static void main(String[] args) throws java.lang.Exception{
-                BufferedReader objReader = new BufferedReader(new FileReader("../../testCases/t3/t3_6.txt"));
+                BufferedReader objReader = new BufferedReader(new FileReader("../../testCases/t3/t3_2.txt"));
                 String str1 = objReader.readLine();
                 String[] strs1 = str1.split(" ");
+		int[] arr = new int[5];
 
+		for(int i=0; i<5; i++){
+			arr[i] = Integer.parseInt(strs1[i]);
+		}
+
+		Arrays.sort(arr);
                 StringBuilder sb = new StringBuilder();
 
-                FileWriter writer = new FileWriter("../../Outputs/o3/o3_6.txt");  
+                FileWriter writer = new FileWriter("../../Outputs/o3/o3_2.txt");  
                 PrintWriter buffer = new PrintWriter(writer);
 
                 long maxSum = 0;
                 long minSum = 0;
 
 		for(int i=0;i<5;i++){
-			int ele = Integer.parseInt(strs1[i]);
+			int ele = arr[i];
 			if(i != 0){
 				maxSum += (long)ele;
 			}
